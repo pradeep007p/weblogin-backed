ffd9eddcb1d5bb0d0f8cac677c9140a50afdb2e8
pradeep007p <pradeepkori007pk@gmail.com>
2025-09-13 18:30:03 +0545

    Update server.js

--- server.js
+++ server.js
@@ -15,3 +15,24 @@ app.get('/', (req, res) => {
 // Start server
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoutes = require("./routes/auth");
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+require("./config/db")(); // DB connect function
+
+app.get("/", (req, res) => {
+  res.send("Backend working with MongoDB!");
+});
+
+// Auth routes
+app.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
