2f710db6ebc938aefd7e2ce2b93ed44a4192d748
pradeep007p <pradeepkori007pk@gmail.com>
2025-09-13 12:07:55 +0545

    Add files via upload

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("✅ WebLogin Backend Working!");
+});
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log("Server running on port " + port);
+});
