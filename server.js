f9f00d8c543ff5347dd3e4533f5a07d6ea2f2df7
pradeep007p <pradeepkori007pk@gmail.com>
2025-09-13 14:32:22 +0545

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./config/db');
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("✅ WebLogin Backend Working!");
+// MongoDB connect
+connectDB();
+
+// Test route
+app.get('/', (req, res) => {
+  res.send('✅ WebLogin Backend Working with MongoDB!');
 });
 
+// Start server
 const port = process.env.PORT || 4000;
-app.listen(port, () => {
-  console.log("Server running on port " + port);
-});
+app.listen(port, () => console.log(`Server running on port ${port}`));
