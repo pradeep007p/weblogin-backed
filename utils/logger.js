60f11c6c9bb6b6fd22af891b94a6b35c9ee71e1c
pradeep007p <pradeepkori007pk@gmail.com>
2025-09-19 18:53:25 +0545

    Delete utils directory

--- utils/logger.js
+++
@@ -1,9 +0,0 @@
-const { createLogger, transports, format } = require('winston');
-
-const logger = createLogger({
-  level: 'info',
-  format: format.combine(format.colorize(), format.simple()),
-  transports: [ new transports.Console() ]
-});
-
-module.exports = logger;
